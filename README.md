3c515c6225901083f807fe86cba28adec5b479f6
bochatayy <yannick.bochatay@meteo.fr>
2018-05-03 14:09:44 +0200

    suppression de bower. Passage en v1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # JSYG.Events
 Events constructor for JSYG framework (or standalone).
+
+
+## Installation
+```shell
+npm install jsyg-events
+```
--- tests/tests.js
+++ tests/tests.js
@@ -10,16 +10,22 @@ if (typeof require!= "undefined") {
     });
 }
 
+QUnit.config.autostart = false;
+
 (function(factory) {
     
-    if (typeof define == 'function' && define.amd) define(["jsyg-events"],factory);
+    if (typeof define == 'function' && define.amd) require(["jsyg-events"],factory);
     else factory(Events);
     
 }(function(Events) {
 
+    QUnit.start();
+
+    const { module, test } = QUnit
+
     module("Events");
 
-    test("Manipulation des événements", function() {     
+    test("Manipulation des événements", assert => {     
         
         var cpt = 0;
         
@@ -29,25 +35,25 @@ if (typeof require!= "undefined") {
         
         events.ontest = null;
         
-        expect(4);
+        assert.expect(4);
         
         events.on("test",incremente);
         events.trigger("test");
-        equal(cpt,1,"Déclenchement de l'événement");
+        assert.equal(cpt,1,"Déclenchement de l'événement");
         
         events.on("test",incremente);
         events.trigger("test");
-        equal(cpt,2,"Non prise en compte des doublons");
+        assert.equal(cpt,2,"Non prise en compte des doublons");
         
         events.off("test",incremente);
         events.trigger("test");
-        equal(cpt,2,"Suppression d'un événement");
+        assert.equal(cpt,2,"Suppression d'un événement");
         
         events.one("test",incremente);
         events.trigger("test");
         events.trigger("test");
         events.trigger("test");
-        equal(cpt,3,"Méthode one qui retire aussitot l'événement");
+        assert.equal(cpt,3,"Méthode one qui retire aussitot l'événement");
         
     });
     
